ef92de09ef9c44cbbba145b92f271ca61bf480c0
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-05 16:45:19 +0900

    feat(Header,index.tsx): トップページの作成 #7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,65 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
-import { Button, Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import Typed from "react-typed";
+import BasePage from "../components/BasePage";
 
 class Index extends React.Component {
+  roles: string[];
+  constructor(props) {
+    super(props);
+    this.roles = [
+      "Developer",
+      "Tech Lover",
+      "Team Player",
+      "Course Creator",
+      "React.js",
+      "Angular",
+    ];
+  }
   render() {
     return (
-      <BaseLayout>
-        <Container>
-          <Button color="danger">削除</Button>
-        </Container>
+      <BaseLayout className="cover">
+        <div className="main-section">
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper`}>
+                    <div className="back">
+                      <img className="image" src="/images/IMG_6179.PNG" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Masaaaki Moriya. This is
+                    the first project created with Next.js.
+                  </h1>
+                </div>
+                <Typed
+                  loop
+                  typeSpeed={70}
+                  backSpeed={70}
+                  strings={this.roles}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                />
+                <div className="hero-welcome-bio">
+                  <h1>Let's take a look on my work.</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </BaseLayout>
     );
   }
