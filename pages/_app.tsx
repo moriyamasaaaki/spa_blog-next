5ce3452b8984cfdc682785cfeccfa126f893bce9
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-06 04:32:03 +0900

    refactor(tsx): コンポーネントの移動と修正 #11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,6 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/main.scss";
 
-class App extends React.Component {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps }: any = this.props;
-    return <Component {...pageProps} />;
-  }
-}
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default App;
