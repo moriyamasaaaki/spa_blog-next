95208d8ff6f66b81abccf929d62d29ba366e9f23
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-09 04:09:58 +0900

    feat(tsx): ファイル追加 #14

--- pages/api/v1/posts/[id].tsx
+++ pages/api/v1/posts/[id].tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 export default async (req, res) => {
   try {
     const axiosRes = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
+      `https://jsonplaceholder.typicode.com/posts/${req.query.id}`
     );
-    const posts = axiosRes.data;
-    res.status(200).json(posts.slice(0, 10));
+    const post = axiosRes.data;
+    res.status(200).json(post);
   } catch (e) {
     console.error(e);
     res.status(e.status || 400).json({ message: "Api error" });
