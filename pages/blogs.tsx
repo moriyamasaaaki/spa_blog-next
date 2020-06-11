90b5eee25dda280bea9ce6a120a8d5be412f5360
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-11 19:36:52 +0900

    feat(tsx): data、loading追加 #18

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,15 +1,16 @@
-import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/BasePage";
+import { useGetUser } from "../actions/user";
 
 const Blogs = () => {
-    return (
-      <BaseLayout>
-        <BasePage>
-          <h1>I am Blog Page</h1>
-        </BasePage>
-      </BaseLayout>
-    );
-}
+  const { data, loading } = useGetUser();
+  return (
+    <BaseLayout user={data} loading={loading}>
+      <BasePage>
+        <h1>I am Blog Page</h1>
+      </BasePage>
+    </BaseLayout>
+  );
+};
 
 export default Blogs;
