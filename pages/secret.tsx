784f8849498f48bc007e15a59222c49caa2873e3
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-14 01:46:35 +0900

    feat(tsx): ページ作成 #22

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -1,28 +1,27 @@
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/BasePage";
 import { useGetUser } from "../actions/user";
-import { useRouter } from "next/router";
+import Redirect from "../components/shared/Redirect";
+import withAuth from "../hoc/secret";
 
-const Secret = () => {
+const Secret = ({ title }) => {
   const { data, loading } = useGetUser();
-  const router = useRouter();
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
   if (!data) {
-    router.push("/api/v1/login");
-    return null;
+    return <Redirect to="/api/v1/login" />;
   } else {
     return (
       <BaseLayout user={data} loading={loading}>
         <BasePage>
-          <h1>I am Secret Page</h1>
+          <h1>I am Secret Page - {title}</h1>
         </BasePage>
       </BaseLayout>
     );
   }
 };
 
-export default Secret;
+export default withAuth(Secret);
