640771986e99faeb908e3aecb8f1d27bb6f3c657
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-15 03:15:21 +0900

    feat(tsx): DBからデータの取得 #24

--- pages/portfolios/index.tsx
+++ pages/portfolios/index.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 import BaseLayout from "../../components/layouts/BaseLayout";
 import BasePage from "../../components/BasePage";
-import { useGetPosts } from "../../actions";
 import { useGetUser } from "../../actions/user";
+import PortfolioApi from "../../lib/api/portfolios";
 
-const Portfolios = () => {
-  const { data, loading, error } = useGetPosts();
+const Portfolios = ({ portfolios }) => {
   const { data: dataU, loading: loadingU } = useGetUser();
 
-  const renderPosts = (posts) => {
-    return posts.map((post) => (
-      <li key={post.id} style={{ fontSize: "20px" }}>
-        <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
-          <a>{post.title}</a>
+  const renderPortfolios = (portfolios) => {
+    return portfolios.map((portfolio) => (
+      <li key={portfolio._id} style={{ fontSize: "20px" }}>
+        <Link as={`/portfolios/${portfolio._id}`} href="/portfolios/[id]">
+          <a>{portfolio.title}</a>
         </Link>
       </li>
     ));
@@ -21,13 +20,18 @@ const Portfolios = () => {
   return (
     <BaseLayout user={dataU} loading={loadingU}>
       <BasePage>
-        <h1>I am Portfolio Page</h1>
-        {loading && <p>Loading data...</p>}
-        {data && <ul>{renderPosts(data)}</ul>}
-        {error && <div className="alert alert-danger">{error.message}</div>}
+        <ul>{renderPortfolios(portfolios)}</ul>
       </BasePage>
     </BaseLayout>
   );
 };
+
+export async function getStaticProps() {
+  const json = await new PortfolioApi().getAll();
+  const portfolios = json.data;
+  return {
+    props: { portfolios },
+  };
+}
 
 export default Portfolios;
