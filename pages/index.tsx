b8186eea7a9f5af594dcb3389ac116fabdbd3b60
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-15 04:08:22 +0900

    refactor(tsx): 基本レイアウトの変更 #24

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,12 @@ const ROLES = [
 const Index = () => {
 const { data, loading } = useGetUser();
   return (
-    <BaseLayout user={data} loading={loading} className="cover">
+    <BaseLayout
+      user={data}
+      loading={loading}
+      navClass="transparent"
+      className="cover"
+    >
       <div className="main-section">
         <Container>
           <Row>
