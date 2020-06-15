597963e9bcf253375bc4a516f664b30c5584bfa7
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-16 02:41:30 +0900

    refactor(tsx): コード修正 #25

--- pages/portfolios/index.tsx
+++ pages/portfolios/index.tsx
@@ -3,33 +3,43 @@ import BaseLayout from "../../components/layouts/BaseLayout";
 import BasePage from "../../components/BasePage";
 import { Row, Col } from "reactstrap";
 import { useGetUser } from "../../actions/user";
+import { useRouter } from 'next/router';
 import PortfolioApi from "../../lib/api/portfolios";
 import PortfolioCard from "../../components/PortfolioCard";
 
 const Portfolios = ({ portfolios }) => {
+  const router = useRouter();
   const { data: dataU, loading: loadingU } = useGetUser();
 
   return (
     <BaseLayout user={dataU} loading={loadingU}>
-      <BasePage header="Portfolios" className="portfolio-page">
+      <BasePage
+        header="Portfolios"
+        className="portfolio-page">
         <Row>
-          {portfolios.map((portfolio) => (
-            <Col key={portfolio._id} md="4">
+          {portfolios.map(portfolio =>
+            <Col
+              key={portfolio._id}
+              onClick={() => {
+                router.push('/portfolios/[id]', `/portfolios/${portfolio._id}`)
+              }}
+              md="4">
               <PortfolioCard portfolio={portfolio} />
             </Col>
-          ))}
+          )
+          }
         </Row>
       </BasePage>
     </BaseLayout>
-  );
-};
+  )
+}
 
 export async function getStaticProps() {
   const json = await new PortfolioApi().getAll();
   const portfolios = json.data;
   return {
-    props: { portfolios },
-  };
+    props: { portfolios }
+  }
 }
 
 export default Portfolios;
