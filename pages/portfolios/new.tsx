a45593ea72585bcfbf59ec2b5101e9121e34cfe5
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-16 15:58:01 +0900

    feat(ProfileForm,new): 型定義 #26

--- pages/portfolios/new.tsx
+++ pages/portfolios/new.tsx
@@ -1,7 +1,4 @@
 
-
-
-
 import BaseLayout from '../../components/layouts/BaseLayout';
 import BasePage from '../../components/BasePage';
 import withAuth from '../../hoc/withAuth';
@@ -9,18 +6,21 @@ import { Row, Col } from 'reactstrap';
 import PortfolioForm from '../../components/PortfolioForm';
 
 const PortfolioNew = ({ user, loading: userLoading }) => {
+  const createPortfolio = (data) => {
+    alert(JSON.stringify(data));
+  };
 
-    return (
-        <BaseLayout user={user} loading={userLoading}>
-            <BasePage header="Create Portfolio">
-                <Row>
-                    <Col md="8">
-                        <PortfolioForm />
-                    </Col>
-                </Row>
-            </BasePage>
-        </BaseLayout>
-    )
-}
+  return (
+    <BaseLayout user={user} loading={userLoading}>
+      <BasePage header="Create Portfolio">
+        <Row>
+          <Col md="8">
+            <PortfolioForm onSubmit={createPortfolio} />
+          </Col>
+        </Row>
+      </BasePage>
+    </BaseLayout>
+  );
+};
 
-export default withAuth(PortfolioNew)('admin');
+export default withAuth(PortfolioNew)("admin");
