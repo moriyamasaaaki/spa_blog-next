d8f4646ee4d682f96ac47e07f975e58c8b2548b5
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-21 02:51:40 +0900

    feat(tsx): エラー処理の追加 #36

--- actions/index.tsx
+++ actions/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const fetcher = (url) =>
-  fetch(url).then(async res => {
+  fetch(url).then(async (res) => {
     const result = await res.json();
 
     if (res.status !== 200) {
@@ -11,26 +11,24 @@ export const fetcher = (url) =>
     }
   });
 
-  
-  export function useApiHandler(apiCall) {
-    const [reqState, setReqState] = useState({
-      error: null,
-      data: null,
-      loading: false,
-    });
+export function useApiHandler(apiCall) {
+  const [reqState, setReqState] = useState({
+    error: null,
+    data: null,
+    loading: false,
+  });
 
-    const handler = async (...data) => {
-      setReqState({ error: null, data: null, loading: true });
-      try {
-        const json = await apiCall(...data);
-        setReqState({ error: null, data: json.data, loading: false });
-      } catch (e) {
-        const message =
-          (e.response && e.response.message) ||
-          "Ooops, something went wrong...";
-        setReqState({ error: message, data: null, loading: false });
-      }
-    };
+  const handler = async (...data) => {
+    setReqState({ error: null, data: null, loading: true });
+    try {
+      const json = await apiCall(...data);
+      setReqState({ error: null, data: json.data, loading: false });
+    } catch (e) {
+      const message =
+        (e.response && e.response.data) || "Ooops, something went wrong...";
+      setReqState({ error: message, data: null, loading: false });
+    }
+  };
 
-    return [handler, { ...reqState }];
-  }
+  return [handler, { ...reqState }];
+}
